6b6c7ce05ad9ed67e26ded68ec4ad8ca28a2ae34
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-27 17:31:27 -0300

    Initial Commit

---
+++ HangingSloth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangingSloth
+//
+//  Created by Rodrigo Cavalcanti on 27/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
